06eef36f08f5751cb9772e5ede5ca354c9fa1300
damon ren <demiao.ren@gmail.com>
2018-11-13 16:24:16 -0800

    add firebase js stuff

---
+++ js/firebase_setup.js
@@ -0,0 +1,16 @@
+
+
+  var config = {
+    apiKey: "AIzaSyAzdyStJSvK2g5poM_TS9VC5jSaiQRCEyY",
+    authDomain: "comp1930-project.firebaseapp.com",
+    databaseURL: "https://comp1930-project.firebaseio.com",
+    projectId: "comp1930-project",
+    storageBucket: "comp1930-project.appspot.com",
+    messagingSenderId: "724816316887"
+  };
+  firebase.initializeApp(config);
+        
+
+  
+  
+
