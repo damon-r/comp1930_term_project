cb996b9ea4efdf8bc5da5eb92e7042452b00128d
damon ren <demiao.ren@gmail.com>
2018-11-21 09:00:18 -0800

    better looking main.html

--- js/main.js
+++ js/main.js
@@ -5,13 +5,18 @@ $(document).ready(function() {
   //var userUid = firebase.auth().currentUser.uid;
   var userUid = '9EqPuCH1fEU4siXeBZyJHSxyS8I3';
   var currentUserRef = database.ref('/users/' + userUid);
-  var monthNames = [
-    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ];
+  
   
   var group_title, group_description, group_createdOn, group_memberCount, currentUserEmail, currentUserDisplayName;
 
+  //sets the user name on the top right cornner.
+  currentUserRef.once('value').then(function(snapshot) {
+      var userName = snapshot.child('displayName').val();
+      $('#profileName').text(userName);
+  });
+  
   //retrieves current user's groups, needs to be run once page is loaded.
+  //!!!!!!!!!!!might need to sort the data first!!!!!!!!!!!!!!
   database.ref('users/' + userUid + '/groups').once('value').then(function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
       var groupUid = childSnapshot.key;
@@ -19,22 +24,52 @@ $(document).ready(function() {
     });
   });
 
-  //remove a table from the page and the database.
+  //click exit buttons will get a warning of quiting a group, if 
+  //"OK" is clicked, remove that group info from the page and the
+  //database.
   $(document).on('click', '.exitButtons', 
-  function removeAGroupTable() {
-    console.log('running');
+  function quitAGroupConfirmation() {
     var classes = $(this).attr('class').split(' ');
     var groupUid = classes[0];
-    $('#' + groupUid).remove();
-    removeGroupInfoFromDatabase(groupUid);
+    if (confirm("Are you sure you want to quit this group?")) {
+      $('#' + groupUid).remove();
+      removeGroupInfoFromDatabase(groupUid);
+    }
   });
 
-
+  //click enter buttons will 
   $(document).on('click', '.enterButtons', 
   function redirect() {
-    window.location.href = "./roomCreation.html";
+    window.location.href = "./chat-room/index.html";
   });
 
+  
+  $('#joinARoom').click(function promptToGetKey(){
+      $('#getKeyModal').css('display', 'block');
+    });
+    
+  $('#closeBtn').click(function closeModal() {
+    $('#getKeyModal').css('display', 'none');
+    $('.errorText').text("");
+  });
+
+  $('#validateBtn').click(function validateKey() {
+    database.ref('groups/').once('value').then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var groupUid = childSnapshot.key;
+        console.log(groupUid);
+        var groupKeyEntered = $('#groupKey').val();
+        console.log(groupKeyEntered);
+        if (groupUid == groupKeyEntered) {
+          $('.errorText').text("");
+          window.location.href="./chat-room/index.html";
+          return;
+        }
+      });
+      $('.errorText').text("The key is not valid.");
+    });
+    
+  });
   // //database.ref('/groups' + groupUid);
   // function writeUserData(inputName, inputDesc, inputImage) {
   //   firebase.database().ref('groups/' + inputName).set({
@@ -44,7 +79,8 @@ $(document).ready(function() {
   //     users: userList
   //   });
   // }
-
+  
+  //createNewGroup();
   //retieve current user's displayName and email.
   function createNewGroup() {
     currentUserRef.once('value').then(function(snapshot) {
@@ -57,13 +93,14 @@ $(document).ready(function() {
     });
   }
 
-  //============creating and writing a new group==================
+  //creates a json object containing all the group info passed in and generates
+  //a random key assigned to that group.
   function updatingNewGroup(newGroup_title, newGroup_description, currentUserEmail, currentUserDisplayName) {
     var currentDate = getCurrentDate();
     // A new group
     var newGroupData = {
       title: newGroup_title,
-      description: newGroup_description, 
+      description: newGroup_description,
       memberCounts: 1,
       dateCreated: currentDate,
       memberInfo: {
@@ -75,17 +112,22 @@ $(document).ready(function() {
     };
     // Get a key for a new group.
     var newGroupUid = rootRef.child('groups').push().key;
-    // Write the new group's data simultaneously in the groups list and the user's group list.
+    // Write the new group's data simultaneously in the groups list and the 
+    //user's group list.
     var updates = {};
     updates['groups/' + newGroupUid] = newGroupData;
     updates['users/' + userUid + '/groups/' + newGroupUid] = true;
     console.log('updatingNewGroup() called.');
     return rootRef.update(updates);
   }
-  //==========end of creating and writing a new group function=============
+  //==========end of the function=============
 
-  //==========get current date function=================
+  //==========get current date function, eg. Nov 20, 2018.=================
   function getCurrentDate() {
+    var monthNames = [
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "October",
+      "Nov", "Dec"
+    ];
     var currentDate = new Date();
     var date = currentDate.getDate();
     var month = currentDate.getMonth();
@@ -95,7 +137,8 @@ $(document).ready(function() {
   //=========end of the function=======================
 
 
-  //remove from database according to groupUid.
+  //removes given groupUid from "users" and "groups", including all the data 
+  //under the groupUid.
   function removeGroupInfoFromDatabase(groupUid) {
     console.log('beginning to remove groups under userUid');
     currentUserRef.child('groups/' + groupUid).remove();
@@ -104,19 +147,25 @@ $(document).ready(function() {
   //=========end of the function=======================
 
 
-  //retrieving information according to groupUid.
+  //retrieves title, description, memberCount and creadtedOn info of a group using //groupUid, adds them onto a table and displays them on page.
   function retrieveGroupInfo(groupUid) {
     database.ref('groups/' + groupUid).once('value').then(function(snapshot) {
+      console.log(groupUid);
       group_title = snapshot.child('title').val();
       group_description = snapshot.child('description').val();
       group_memberCount = snapshot.child('memberCounts').val();
       group_createdOn = snapshot.child('dateCreated').val();
+      console.log('group Info Retieved..');
       addAGroupTable(groupUid, group_title, group_description, group_memberCount, group_createdOn);
     });
   }
-
-  //=======function of creating and appending a group table.========
-  function addAGroupTable(groupUid, group_title, group_description, group_memberCount, group_createdOn) {
+  //===========end of the function.==============
+  
+  //=======function for creating and appending a group table.========
+  $('#roomSpace').text("");
+  function addAGroupTable(groupUid, group_title, group_description, 
+    group_memberCount, group_createdOn) {
+      
     var table = $('<table></table>');
     table.attr('id', groupUid);
     var tr_groupTitle = $('<tr></tr>');
@@ -159,8 +208,9 @@ $(document).ready(function() {
     td_exit.append(exit_button);
     tr_footer.append(td_enter, td_exit);
     table.append(tr_groupTitle, tr_groupInfo, tr_footer);
-    $('#roomSpace').append(table);
+    $('#roomSpace').prepend(table);
+    
   }
-  //=======end of the function.========
+  //===========end of the function.==============
 })
 
