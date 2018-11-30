e5b4ff4e00de091d33934a991b2e26f2423a4915
damon ren <demiao.ren@gmail.com>
2018-11-30 15:34:48 -0800

    separated js from roomCreation.html

--- js/room_creation.js
+++ js/room_creation.js
@@ -1,133 +1,123 @@
-
 $(document).ready(function() {
-  // = = = = = Define Variables = = = = =
-  var user;
-  var currentUserEmail;
-  var userList = {};
-  const database = firebase.database();
-  
-  //listens for user authentication status change.
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      var currentUserUid = user.uid;
-      var currentUserRef = database.ref('users/' + currentUserUid);
-      setUsername(currentUserRef);
-      console.log('currentUserUid: ' + currentUserUid);
-    } else {
+    // = = = = = Define Variables = = = = =
+    var user;
+    var userCount;
+    var currentUserEmail;
+    var userList;
 
-      console.log('user not logged in');
-    }
-  });
+    //Add yourself to the array.
+    setTimeout(function() {
+        user = firebase.auth().currentUser.displayName;
+        userCount = 1; //How many users are in the room. Defaults to 1, because you're kinda making it.
+        currentUserDisplay = firebase.auth().currentUser.displayName; //This is your username. Hi!
+        userList = new Array(); //An array to hold usernames.
+        $("#profileName").text(currentUserDisplay);
+    }, 500);
 
+    // = = = = = Button Events = = = = =
+    //Spawn new list items on click, assuming a name is typed.
+    $("#addPerson").click(function(event){
+        var nameOfAdded = $("#personToAdd").val();
 
-  // //Add yourself to the array.
-  // setTimeout(function() {
-  //   var uid = firebase.auth().currentUser.uid;
-  //   userCount = 1; //How many users are in the room. Defaults to 1, because you're kinda making it.
-  //   currentUserRef = database.ref('users/' + uid);
-  //   currentUserDisplay = setUsername(currentUserRef);
-  //   userList = new Array(currentUserDisplay); //An array to hold usernames.
-  //   console.log(currentUserDisplay);
-  //   console.log(userList);
-  // }, 500);
-  // console.log(userCount +  userList);
-
-  //sets the user name on the top right cornner.
-  function setUsername(currentUserRef) {
-    currentUserRef.once('value').then(function(snapshot) {
-        var userName = snapshot.child('displayName').val();
-        $('#profileName').text(userName);
-        return userName;
+        if (nameOfAdded != "") {
+            userCount += 1;
+            userList.push(nameOfAdded);
+            $("#userList").append("<div class='person'><p class='personName'>" + nameOfAdded + "</p><div class='deleteButton'><span>&times</span></div></div>");
+            $("#personToAdd").val("");
+       } else {
+            alert("Please provide a user to add.");
+        }
     });
-  }
-  
-  // = = = = = Button Events = = = = =
-  //Spawn new list items on click, assuming a name is typed.
-  $("#addPerson").click(function() {
-    var nameOfAdded = $("#personToAdd").val();
-    if ($.trim(nameOfAdded).length != 0) {
-      $('#error').text('');
-      userList[nameOfAdded] = true;
-      $("#userList").append(
-        "<div class='person'>" +
-        "<p class='personName'>"+ nameOfAdded + "</p>" +
-          "<div class='deleteButton'><span>&times</span></div></div>");
-          
-      $("#personToAdd").val("");
-    } else {
-        $('#error').text('Please enter an email address.');
+
+    //Alternatively for #addPerson, listen for enter presses.
+    $("#personToAdd").on("keypress", function(e) {
+        if (e.which === 13) {
+        $("#addPerson").click();
+        }
+    });
+
+    //Delete stuff on delete button press.
+    $(document).on("click", ".deleteButton", function() {
+        var nameWithin = $(this).closest(".person").children("p").text();
+        var position = $.inArray(nameWithin, userList, 0);
+
+        //Edit array.
+        userList[position] = "";
+        userList = userList.filter(trimArray)
+
+        $(this).closest(".person").remove();
+    });
+
+     //Create room on click.
+    $("#createRoom").click(function(event){
+
+        //Check if the name is filled.
+        if ($("#roomName").val() != "") {
+            let inputName = $("#roomName").val();
+            let inputDesc = $("#roomDesc").val();
+            let inputColor = $("#roomColor").val();
+            writeUserData(inputName, inputDesc, inputColor);
+            setTimeout(function() {
+                window.location.href = "main.html";
+            }, 1000);
+        } else {
+            alert("Your room requires a name!");
+        }
+    });
+
+    // = = = = = Add to Database, Join Room = = = = =
+    //Adds the room to the database, and adds the person to the room.
+    function writeUserData(inputName, inputDesc, inputColor) {
+        var database = firebase.database().ref();
+        var newGroupUid = database.child('groups').push().key;
+        firebase.database().ref('groups/' + newGroupUid).set({
+            dateCreated: getCurrentDate(),
+            title: inputName,
+            description: inputDesc,
+            roomColor: inputColor,
+            memberCounts: userCount,
+            memberInfo: userList
+        });
+        
+        //Write the new group's data simultaneously in the groups list and the 
+        //user's group list.
+        var updates = {};
+        var uid = firebase.auth().currentUser.uid;
+        console.log("Current ID: ", uid);
+        console.log("Room ID: ", newGroupUid);
+        updates['users/' + uid + '/groups/' + newGroupUid] = true;
+        database.update(updates);
+        
+        //Write invited users in as well
+        var ref = firebase.database().ref('users');
+        for (let user of userList) {
+            ref.orderByChild('email').equalTo(user).on("child_added", function(snapshot) {
+                var uidOther = snapshot.key;
+                var updates = {};
+                updates['users/' + uidOther + '/groups/' + newGroupUid] = true;
+                database.update(updates);
+            });
+        } 
     }
-    console.log(userList);
-  });
 
-  //Alternatively for #addPerson, listen for enter presses.
-  $("#personToAdd").on("keypress", function(e) {
-      if (e.which === 13) {
-      $("#addPerson").click();
-      }
-  });
+    //==========get current date function, eg. Nov 20, 2018.=================
+    function getCurrentDate() {
+        var monthNames = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
+        "Nov", "Dec"
+        ];
+        var currentDate = new Date();
+        var date = currentDate.getDate();
+        var month = currentDate.getMonth();
+        var year = currentDate.getFullYear();
+        console.log("test");
+        return monthNames[month] + " " + date + ", " + year;
+    }
 
-  //Delete stuff on delete button press.
-  $(document).on("click", ".deleteButton", function() {
-      var nameWithin = $(this).closest(".person").children("p").text();
-      //var position = $.inArray(nameWithin, userList, 0);
+    // = = = = = Trim Array = = = = =
+    //A thing used to determine what to put in the new array.
+    function trimArray(name) {
+        return name != "";
+    }
 
-      //Edit array.
-      // userList[position] = "";
-      // userList = userList.filter(trimArray);
-      delete userList[nameWithin];
-      console.log(userList);
-      $(this).closest(".person").remove();
-  });
-
-    //Create room on click.
-  $("#createRoom").click(function(event){
-
-      //Check if the name is filled.
-      if ($("#roomName").val() != "") {
-          let inputName = $("#roomName").val();
-          let inputDesc = $("#roomDesc").val();
-          let inputImage = $("#roomImage").val();
-          writeUserData(inputName, inputDesc, inputImage);
-          
-      } else {
-          alert("Your room requires a name!");
-      }
-  });
-
-  // = = = = = Add to Database = = = = =
-  //Adds the room to the database.
-  function writeUserData(inputName, inputDesc, inputImage) {
-      var newGroupUid = database.ref().child('groups').push().key;
-      console.log(newGroupUid);
-      firebase.database().ref('groups/' + newGroupUid).set({
-          dateCreated: getCurrentDate(),
-          title: inputName,
-          description: inputDesc,
-          roomImage: inputImage,
-          memberCounts: userList.length + 1,
-          memberInfo: userList,
-      });
-      window.location.href="./chat_room.html#" + newGroupUid;
-  }
-
-  //==========get current date function, eg. Nov 20, 2018.=================
-  function getCurrentDate() {
-      var monthNames = [
-      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
-      "Nov", "Dec"
-      ];
-      var currentDate = new Date();
-      var date = currentDate.getDate();
-      var month = currentDate.getMonth();
-      var year = currentDate.getFullYear();
-      return monthNames[month] + " " + date + ", " + year;
-  }
-
-  // // = = = = = Trim Array = = = = =
-  // //A thing used to determine what to put in the new array.
-  // function trimArray(name) {
-  //     return name != "";
-  // }
-
-});
+    });
